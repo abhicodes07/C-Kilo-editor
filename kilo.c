55ca70eff375eced8ccc84e1f97420ecb57972af
Abhinav <abhiasthana76@gmail.com>
2024-09-01 19:56:39 +0530

    Add README

--- kilo.c
+++ kilo.c
@@ -1,7 +1,33 @@
+#include <asm-generic/termbits.h>
+#include <bits/termios_inlines.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
-  char c;
+// ==================================================================
+// disabling raw mode
+void disableRawMode(void){
+
+}
+
+// ==================================================================
+// enabling raw mode
+void enableRawMode(void){
+  struct termios raw; // struct to store the terminal attributes read by tcgetattr
+
+  tcgetattr(STDIN_FILENO, &raw); // reads terminal attributes
+
+  raw.c_lflag &= ~(ECHO); // reverses the bits of echo i.e., flipping bits 
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // apply terminal attributes
+}
+
+// ==================================================================
+// main
+int main(void){
+  enableRawMode();
+
+  char c; // stores input from the keyboard
   // asking read() to 1 byte from the standard input into the variable c
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
