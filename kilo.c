649982bda361c395a89133fa4ea99edc0a8d82bc
Abhinav <abhiasthana76@gmail.com>
2024-10-08 21:48:50 +0530

    Display the proper number of tildes

--- kilo.c
+++ kilo.c
@@ -128,7 +128,7 @@ void editorProcessKeypress(void) {
 
 void editorDrawRows(void) {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
