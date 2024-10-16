d26049279830e84330b123b71f25e3a645cc149c
Abhinav <abhiasthana76@gmail.com>
2024-10-16 21:45:35 +0530

    Display right number of tildes, hard way

--- kilo.c
+++ kilo.c
@@ -1,5 +1,6 @@
 /*** includes ***/
 
+#include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -87,10 +88,38 @@ char editorReadKey(void) {
   return c;
 }
 
+/* get cursor position */
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+
+  return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return getCursorPosition(rows, cols);
+    editorReadKey();
     return -1;
   } else {
     *cols = ws.ws_col;
